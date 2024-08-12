560bad8756d4c389426e0b4f4f08064c56183d81
AzizZoom <azizzoomkha@gmail.com>
2024-08-12 12:49:57 +0530

    final update

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const nav5 = document.getElementById('nav-5');
 const nav6 = document.getElementById('nav-6');
 const navItems = [nav1, nav2, nav3, nav4, nav5, nav6];
 
+const projectItem = document.querySelectorAll('.projectItem');
+const screenWidth = window.screen.width;
+
 // Animate Nav Items In/Out
 function navAnimation (direction1, direction2) {
     navItems.forEach((nav, i) => {
@@ -34,8 +37,57 @@ function toggleNav() {
     }
 }
 
+// Function to check if the Grid is in the viewport (Mobile)
+function isGridInViewport(item) {
+    const rect = item.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Play video when it's Grid comes into the viewport (Mobile)
+function checkVideosInView() {
+    projectItem.forEach(item => {
+        const video = item.querySelector('.projectVideo');
+        if (isGridInViewport(item)) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    });
+}
+
+// Check If The Device is a Mobile/Tablet or a Desktop
+function isMobile() {
+    // If screenWidth is less than 1025, it is a mobile. Else it is a desktop
+    if(screenWidth < 1025) {
+        // Add event listeners for scroll and resize (Mobile)
+        window.addEventListener('scroll', checkVideosInView);
+        window.addEventListener('resize', checkVideosInView);
+        // Initial check
+        checkVideosInView();
+    } else {
+        // Add event listeners for mouseenter and mouseleave (Desktop)
+        projectItem.forEach(item => {
+            const video = item.querySelector('.projectVideo');
+            // Play Video On Hover
+            item.addEventListener('mouseenter', () => {
+                video.play();
+            });
+            // Reset Video On Hover
+            item.addEventListener('mouseleave', () => {
+                video.pause();
+            });
+        });
+    }
+}
+
 // Event Listeners
 menuBars.addEventListener('click', toggleNav);
 navItems.forEach((nav) => {
     nav.addEventListener('click', toggleNav);
 });
+isMobile();
